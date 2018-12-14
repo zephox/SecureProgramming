65c39b6805d9a664f9e99e607f5112dfd432992e
Alex <alexiakab@hotmail.com>
2018-12-14 11:02:10 +0100

    initial commit

---
+++ IrunT/IrunT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IrunT
+//
+//  Created by Alex Iakab on 14/12/2018.
+//  Copyright © 2018 Alex Iakab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
